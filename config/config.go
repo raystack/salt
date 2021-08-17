edfc10d0f5fe03dd96867c2d7d229966702779dc
Rohil Surana <rohilsurana96@gmail.com>
2021-08-17 21:06:36 +0530

    refactor: improve error handling and add comments to exported functions for config package

--- config/config.go
+++ config/config.go
@@ -17,42 +17,58 @@ type Loader struct {
 
 type LoaderOption func(*Loader)
 
+// WithViper sets the given viper instance for loading configs
+// instead of the default configured one
 func WithViper(in *viper.Viper) LoaderOption {
 	return func(l *Loader) {
 		l.v = in
 	}
 }
 
+// WithConfigName sets the file name of the config file without
+// the extension
 func WithConfigName(in string) LoaderOption {
 	return func(l *Loader) {
 		l.v.SetConfigName(in)
 	}
 }
 
+// AddConfigPath adds config path to search the config file in,
+// can be used multiple times to add multiple paths to search
 func AddConfigPath(in string) LoaderOption {
 	return func(l *Loader) {
 		l.v.AddConfigPath(in)
 	}
 }
 
+// WithConfigType sets the type of the configuration e.g. "json",
+// "yaml", "hcl"
+// Also used for the extension of the file
 func WithConfigType(in string) LoaderOption {
 	return func(l *Loader) {
 		l.v.SetConfigType(in)
 	}
 }
 
+// WithEnvPrefix sets the prefix for keys when checking for configs
+// in environment variables. Internally concatenates with keys
+// with `_` in between
 func WithEnvPrefix(in string) LoaderOption {
 	return func(l *Loader) {
 		l.v.SetEnvPrefix(in)
 	}
 }
 
+// WithEnvKeyReplacer sets the `old` string to be replaced with
+// the `new` string environmental variable to a key that does
+// not match it.
 func WithEnvKeyReplacer(old string, new string) LoaderOption {
 	return func(l *Loader) {
 		l.v.SetEnvKeyReplacer(strings.NewReplacer(old, new))
 	}
 }
 
+// NewLoader returns a config loader with given LoaderOption(s)
 func NewLoader(options ...LoaderOption) *Loader {
 	loader := &Loader{
 		v: getViperWithDefaults(),
@@ -66,50 +82,51 @@ func NewLoader(options ...LoaderOption) *Loader {
 
 // Load loads configuration into the given mapstructure (https://github.com/mitchellh/mapstructure)
 // from a config.yaml file and overrides with any values set in env variables
-func (l *Loader) Load(config interface{}) {
-	verifyParamIsPtrToStructElsePanic(config)
+func (l *Loader) Load(config interface{}) error {
+	if err := verifyParamIsPtrToStructElsePanic(config); err != nil {
+		return err
+	}
 
 	l.v.AutomaticEnv()
 
-	err := l.v.ReadInConfig()
-	if err != nil {
+	if err := l.v.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			panicf("unable to read configs using viper: %v\n", err)
+			return fmt.Errorf("unable to read configs using viper: %v", err)
 		}
 	}
 
-	err, configKeys := getFlattenedStructKeys(config)
+	configKeys, err := getFlattenedStructKeys(config)
 	if err != nil {
-		panicf("unable to get all config keys from struct: %v\n", err)
+		return fmt.Errorf("unable to get all config keys from struct: %v", err)
 	}
 
-	// Bind each conf fields to environment vars
+	// Bind each conf fields from struct to environment vars
 	for key := range configKeys {
-		err := l.v.BindEnv(configKeys[key])
-		if err != nil {
-			panicf("unable to bind env keys: %v\n", err)
+		if err := l.v.BindEnv(configKeys[key]); err != nil {
+			return fmt.Errorf("unable to bind env keys: %v", err)
 		}
 	}
 
 	// set defaults using the default struct tag
 	defaults.SetDefaults(config)
 
-	err = l.v.Unmarshal(config)
-	if err != nil {
-		panicf("unable to load config to struct: %v\n", err)
+	if err := l.v.Unmarshal(config); err != nil {
+		return fmt.Errorf("unable to load config to struct: %v", err)
 	}
+	return nil
 }
 
-func verifyParamIsPtrToStructElsePanic(param interface{}) {
+func verifyParamIsPtrToStructElsePanic(param interface{}) error {
 	value := reflect.ValueOf(param)
 	if value.Kind() != reflect.Ptr {
-		panicf("Require Ptr to a Struct for Load. Got %v\n", value.Kind())
+		return fmt.Errorf("require ptr to a struct for Load. Got %v", value.Kind())
 	} else {
 		value = reflect.Indirect(value)
 		if value.Kind() != reflect.Struct {
-			panicf("Require Ptr to a Struct for Load. Got Ptr to %v\n", value.Kind())
+			return fmt.Errorf("require ptr to a struct for Load. got ptr to %v", value.Kind())
 		}
 	}
+	return nil
 }
 
 func getViperWithDefaults() *viper.Viper {
@@ -121,16 +138,15 @@ func getViperWithDefaults() *viper.Viper {
 	return v
 }
 
-func getFlattenedStructKeys(config interface{}) (error, []string) {
+func getFlattenedStructKeys(config interface{}) ([]string, error) {
 	var structMap map[string]interface{}
-	err := mapstructure.Decode(config, &structMap)
-	if err != nil {
-		return err, nil
+	if err := mapstructure.Decode(config, &structMap); err != nil {
+		return nil, err
 	}
 
 	flat, err := flatten.Flatten(structMap, "", flatten.DotStyle)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 
 	keys := make([]string, 0, len(flat))
@@ -138,9 +154,5 @@ func getFlattenedStructKeys(config interface{}) (error, []string) {
 		keys = append(keys, k)
 	}
 
-	return nil, keys
-}
-
-func panicf(format string, a ...interface{}) {
-	panic(fmt.Sprintf(format, a...))
+	return keys, nil
 }
