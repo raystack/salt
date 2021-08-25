10f4721584ad97a4f0ce456e3312e5ad2b81706c
Aditya Singh Sisodiya <onlytoaditya@gmail.com>
2021-08-25 16:55:11 +0530

    feat: support for release version info

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/odpf/salt
 go 1.16
 
 require (
+	github.com/gorilla/mux v1.8.0
+	github.com/hashicorp/go-version v1.3.0
 	github.com/jeremywohl/flatten v1.0.1
 	github.com/mcuadros/go-defaults v1.2.0
 	github.com/mitchellh/mapstructure v1.4.1
-	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/muesli/termenv v0.9.0
 	github.com/olekukonko/tablewriter v0.0.5
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
---
+++ version/release_test.go
@@ -0,0 +1,104 @@
+package version_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/odpf/salt/version"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGithubInfo(t *testing.T) {
+	muxRouter := mux.NewRouter()
+	server := httptest.NewServer(muxRouter)
+
+	t.Run("should check for latest version availability by extracting correct version tag for valid json response on release URL", func(t *testing.T) {
+		muxRouter.HandleFunc("/latest", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Header().Set("Content-Type", "application/json")
+			response, _ := json.Marshal(struct {
+				TagName string `json:"tag_name"`
+			}{
+				TagName: "v0.0.2",
+			})
+			rw.Write(response)
+		})
+		info, err := version.ReleaseInfo("http://" + server.Listener.Addr().String() + "/latest")
+		assert.Nil(t, err)
+		assert.Equal(t, "v0.0.2", info.Version)
+		info, err = version.ReleaseInfo("http://" + server.Listener.Addr().String() + "/latest")
+		assert.Nil(t, err)
+		assert.NotEqual(t, "v0.0.1", info.Version)
+	})
+}
+func TestIsCurrentLatest(t *testing.T) {
+	muxRouter := mux.NewRouter()
+	server := httptest.NewServer(muxRouter)
+
+	t.Run("should return true for current version  as the latest version ", func(t *testing.T) {
+		muxRouter.HandleFunc("/latest", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Header().Set("Content-Type", "application/json")
+			response, _ := json.Marshal(struct {
+				TagName string `json:"tag_name"`
+			}{
+				TagName: "v0.0.2",
+			})
+			rw.Write(response)
+		})
+		info, err := version.ReleaseInfo("http://" + server.Listener.Addr().String() + "/latest")
+		assert.Nil(t, err)
+		assert.Equal(t, "v0.0.2", info.Version)
+		res, err := version.IsCurrentLatest("v0.0.2", info.Version)
+		assert.Nil(t, err)
+		assert.True(t, res)
+	})
+	t.Run("should return false for current version not same as the latest version", func(t *testing.T) {
+		muxRouter.HandleFunc("/latest", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Header().Set("Content-Type", "application/json")
+			response, _ := json.Marshal(struct {
+				TagName string `json:"tag_name"`
+			}{
+				TagName: "v0.0.2",
+			})
+			rw.Write(response)
+		})
+		info, err := version.ReleaseInfo("http://" + server.Listener.Addr().String() + "/latest")
+		assert.Nil(t, err)
+		assert.Equal(t, "v0.0.2", info.Version)
+		res, err := version.IsCurrentLatest("v0.0.1", info.Version)
+		assert.Nil(t, err)
+		assert.False(t, res)
+		res, err = version.IsCurrentLatest("", info.Version)
+		assert.NotNil(t, err)
+		assert.False(t, res)
+		res, err = version.IsCurrentLatest("v0.0.3", "")
+		assert.NotNil(t, err)
+		assert.False(t, res)
+	})
+}
+
+func TestUpdateMsg(t *testing.T) {
+	muxRouter := mux.NewRouter()
+	server := httptest.NewServer(muxRouter)
+	t.Run("basic check for notify latest version", func(t *testing.T) {
+		muxRouter.HandleFunc("/latest", func(rw http.ResponseWriter, r *http.Request) {
+			rw.Header().Set("Content-Type", "application/json")
+			response, _ := json.Marshal(struct {
+				TagName string `json:"tag_name"`
+			}{
+				TagName: "v0.0.1",
+			})
+			rw.Write(response)
+		})
+		info, err := version.ReleaseInfo("http://" + server.Listener.Addr().String() + "/latest")
+		assert.Nil(t, err)
+		assert.Equal(t, "v0.0.1", info.Version)
+		res, err := version.IsCurrentLatest("v0.0.1", info.Version)
+		assert.Nil(t, err)
+		assert.True(t, res)
+		s := version.UpdateMsg("v0.0.1", "odpf/optimus")
+		assert.NotEqual(t, "v0.0.1", s)
+	})
+}
