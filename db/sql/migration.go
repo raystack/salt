a59d332e8cb8da61808ddd8fe251f4b8452d781e
Kartik Verma <kartik.verma@go-jek.com>
2021-10-21 10:54:34 +0530

    add: generic package for sql

---
+++ db/sql/migration.go
@@ -0,0 +1,75 @@
+package sql
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(config Config, path ...string) error {
+	m, err := getMigrationInstance(config, path)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err == migrate.ErrNoChange || err == nil {
+		return nil
+	}
+
+	return err
+}
+
+func RunRollback(config Config, path ...string) error {
+	m, err := getMigrationInstance(config, path)
+	if err != nil {
+		return err
+	}
+
+	err = m.Steps(-1)
+	if err == migrate.ErrNoChange || err == nil {
+		return nil
+	}
+
+	return err
+}
+
+func getMigrationInstance(config Config, path []string) (*migrate.Migrate, error) {
+	db, err := sql.Open(config.Driver, config.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	driver, err := getDatabaseAccessObject(db, config.Driver)
+	if err != nil {
+		return nil, err
+	}
+
+	migrationPath := "file://./migrations"
+	if len(path) >= 1 && path[0] != "" {
+		migrationPath = path[0]
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(migrationPath, config.Driver, driver)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func getDatabaseAccessObject(db *sql.DB, driver string) (database.Driver, error) {
+	switch driver {
+	case "postgres":
+		return postgres.WithInstance(db, &postgres.Config{})
+	case "mysql":
+		return mysql.WithInstance(db, &mysql.Config{})
+	default:
+		return nil, errors.New("driver not found")
+	}
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.16
 
 require (
 	github.com/charmbracelet/glamour v0.3.0
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.15.1
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
 	github.com/hashicorp/go-version v1.3.0
 	github.com/jeremywohl/flatten v1.0.1
+	github.com/jmoiron/sqlx v1.3.4
 	github.com/mcuadros/go-defaults v1.2.0
 	github.com/mitchellh/mapstructure v1.4.1
 	github.com/muesli/termenv v0.9.0
@@ -24,6 +27,6 @@ require (
 	github.com/stretchr/testify v1.7.0
 	go.buf.build/odpf/gw/odpf/proton v1.1.9
 	go.uber.org/zap v1.19.0
-	google.golang.org/grpc v1.40.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	google.golang.org/grpc v1.41.0
 )
