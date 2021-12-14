9a9a0943d7c2a6da1c9f4937fc8c5aba4c8fb88c
Sushmith <94100678+aok1j1@users.noreply.github.com>
2021-12-14 21:16:10 +0530

    feat: enable gateway options (#20)
    * chore: add vendor folder to gitignore
    
    * feat: enable gateway to take multiple options
    
    * feat(server): abstract grpc gateway options

--- server/gw.go
+++ server/gw.go
@@ -16,12 +16,43 @@ type GRPCGateway struct {
 	address string
 }
 
+// GatewayOption sets configs, properties or other parameters for the server.GRPCGateway
+type GatewayOption func(*gatewayOptions)
+
+type gatewayOptions struct {
+	gwMuxOptions []runtime.ServeMuxOption
+	gwmux        *runtime.ServeMux
+}
+
+// WithGatewayMuxOptions sets []runtime.ServeMuxOption for server.GRPCGateway
+func WithGatewayMuxOptions(opts ...runtime.ServeMuxOption) GatewayOption {
+	return func(gwo *gatewayOptions) {
+		gwo.gwMuxOptions = opts
+	}
+}
+
+// WithGRPCGateway sets runtime.ServeMux instance for server.GRPCGateway
+func WithGRPCGateway(gwmux *runtime.ServeMux) GatewayOption {
+	return func(gwo *gatewayOptions) {
+		gwo.gwmux = gwmux
+	}
+}
+
 // NewGateway creates a new server.GRPCGateway to proxy grpc requests to specified host and port
-func NewGateway(host string, port int) (*GRPCGateway, error) {
-	return &GRPCGateway{
-		gwmux:   runtime.NewServeMux(),
-		address: fmt.Sprintf("%s:%d", host, port),
-	}, nil
+func NewGateway(host string, port int, opts ...GatewayOption) (*GRPCGateway, error) {
+	gwo := &gatewayOptions{}
+	for _, opt := range opts {
+		opt(gwo)
+	}
+
+	gateway := &GRPCGateway{address: fmt.Sprintf("%s:%d", host, port)}
+	if gwo.gwmux != nil {
+		gateway.gwmux = gwo.gwmux
+	} else {
+		gateway.gwmux = runtime.NewServeMux(gwo.gwMuxOptions...)
+	}
+
+	return gateway, nil
 }
 
 // RegisterHandler helps in adding routes and handlers to be used for proxying requests to grpc service given the grpc-gateway generated Register*ServiceHandlerFromEndpoint function
