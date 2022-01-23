36df4f993e889672f2ea81b4106c101c3ece67c4
Ravi Suhag <suhag.ravi@gmail.com>
2022-01-22 18:15:49 -0800

    feat(printer): add progress bar and update spinner api (#25)

---
+++ printer/spinner.go
@@ -0,0 +1,34 @@
+package printer
+
+import (
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/odpf/salt/term"
+)
+
+type Indicator struct {
+	spinner *spinner.Spinner
+}
+
+func (s *Indicator) Stop() {
+	if s.spinner == nil {
+		return
+	}
+	s.spinner.Stop()
+}
+
+func Spin(label string) *Indicator {
+	set := spinner.CharSets[11]
+	if !term.IsTTY() {
+		return &Indicator{}
+	}
+	s := spinner.New(set, 120*time.Millisecond, spinner.WithColor("fgCyan"))
+	if label != "" {
+		s.Prefix = label + " "
+	}
+
+	s.Start()
+
+	return &Indicator{s}
+}
