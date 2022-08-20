41938c7ad426978022ea991c7e5dd953dea48638
Ravi Suhag <suhag.ravi@gmail.com>
2022-08-20 16:03:30 -0500

    feat: update command grouping

--- cmdx/shell.go
+++ cmdx/shell.go
@@ -7,53 +7,54 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// SetCompletionCmd is used to generate the completion script
-// in bash, zsh, fish, and powershell.
-// This should be added on the root command and can
-// be used as `completion bash` or `completion zsh`.
+// SetCompletionCmd is used to generate the completion script in
+// bash, zsh, fish, and powershell. It should be added on the root
+// command and can be used as `completion bash` or `completion zsh`.
 func SetCompletionCmd(exec string) *cobra.Command {
+	summary := heredoc.Docf(`To load completions:
+		
+		Bash:
+
+		  $ source <(%s completion bash)
+
+		  # To load completions for each session, execute once:
+		  # Linux:
+		  $ %s completion bash > /etc/bash_completion.d/%s
+		  # macOS:
+		  $ %s completion bash > /usr/local/etc/bash_completion.d/%s
+
+		Zsh:
+
+		  # If shell completion is not already enabled in your environment,
+		  # you will need to enable it.  You can execute the following once:
+
+		  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+		  # To load completions for each session, execute once:
+		  $ %s completion zsh > "${fpath[1]}/_yourprogram"
+
+		  # You will need to start a new shell for this setup to take effect.
+
+		fish:
+
+		  $ %s completion fish | source
+
+		  # To load completions for each session, execute once:
+		  $ %s completion fish > ~/.config/fish/completions/%s.fish
+
+		PowerShell:
+
+		  PS> %s completion powershell | Out-String | Invoke-Expression
+
+		  # To load completions for every new session, run:
+		  PS> %s completion powershell > %s.ps1
+		  # and source this file from your PowerShell profile.
+	`, exec)
+
 	return &cobra.Command{
-		Use:   "completion [bash|zsh|fish|powershell]",
-		Short: "Generate shell completion scripts",
-		Long: heredoc.Docf(`To load completions:
-	
-			Bash:
-
-			  $ source <(%s completion bash)
-
-			  # To load completions for each session, execute once:
-			  # Linux:
-			  $ %s completion bash > /etc/bash_completion.d/%s
-			  # macOS:
-			  $ %s completion bash > /usr/local/etc/bash_completion.d/%s
-
-			Zsh:
-
-			  # If shell completion is not already enabled in your environment,
-			  # you will need to enable it.  You can execute the following once:
-
-			  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-			  # To load completions for each session, execute once:
-			  $ %s completion zsh > "${fpath[1]}/_yourprogram"
-
-			  # You will need to start a new shell for this setup to take effect.
-
-			fish:
-
-			  $ %s completion fish | source
-
-			  # To load completions for each session, execute once:
-			  $ %s completion fish > ~/.config/fish/completions/%s.fish
-
-			PowerShell:
-
-			  PS> %s completion powershell | Out-String | Invoke-Expression
-
-			  # To load completions for every new session, run:
-			  PS> %s completion powershell > %s.ps1
-			  # and source this file from your PowerShell profile.
-		`, exec),
+		Use:                   "completion [bash|zsh|fish|powershell]",
+		Short:                 "Generate shell completion scripts",
+		Long:                  summary,
 		DisableFlagsInUseLine: true,
 		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
 		Args:                  cobra.ExactValidArgs(1),
