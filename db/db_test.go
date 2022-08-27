54d54c0a8c3e5cd13d9994ab4224a39a08912ff6
Ravi Suhag <suhag.ravi@gmail.com>
2022-08-26 21:45:14 -0500

    test: add test for WithTx

--- db/db_test.go
+++ db/db_test.go
@@ -1,15 +1,19 @@
 package db_test
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/odpf/salt/db"
 	"github.com/ory/dockertest"
 	"github.com/ory/dockertest/docker"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -21,6 +25,14 @@ const (
 	port     = "5432"
 	dsn      = "postgres://%s:%s@localhost:%s/%s?sslmode=disable"
 )
+
+var (
+	createTableQuery = "CREATE TABLE IF NOT EXISTS users (id VARCHAR(36) PRIMARY KEY, name VARCHAR(50))"
+	dropTableQuery   = "DROP TABLE IF EXISTS users"
+	checkTableQuery  = "SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'users');"
+)
+
+var client *db.Client
 
 func TestMain(m *testing.M) {
 	pool, err := dockertest.NewPool("")
@@ -60,8 +72,6 @@ func TestMain(m *testing.M) {
 
 	pool.MaxWait = 60 * time.Second
 
-	var client *db.Client
-
 	dsn := fmt.Sprintf(dsn, user, password, port, database)
 	var (
 		pgConfig = db.Config{
@@ -88,4 +98,79 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(code)
+}
+
+func TestWithTxn(t *testing.T) {
+	if _, err := client.Exec(dropTableQuery); err != nil {
+		log.Fatalf("Could not cleanup: %s", err)
+	}
+	err := client.WithTxn(context.Background(), sql.TxOptions{}, func(tx *sqlx.Tx) error {
+		if _, err := tx.Exec(createTableQuery); err != nil {
+			return err
+		}
+		if _, err := tx.Exec(dropTableQuery); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Table should be dropped
+	var tableExist bool
+	result := client.QueryRow(checkTableQuery)
+	result.Scan(&tableExist)
+	assert.Equal(t, false, tableExist)
+}
+
+func TestWithTxnCommit(t *testing.T) {
+	if _, err := client.Exec(dropTableQuery); err != nil {
+		log.Fatalf("Could not cleanup: %s", err)
+	}
+	query2 := "SELECT 1"
+
+	err := client.WithTxn(context.Background(), sql.TxOptions{}, func(tx *sqlx.Tx) error {
+		if _, err := tx.Exec(createTableQuery); err != nil {
+			return err
+		}
+		if _, err := tx.Exec(query2); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	// WithTx should not return an error
+	assert.NoError(t, err)
+
+	// User table should exist
+	var tableExist bool
+	result := client.QueryRow(checkTableQuery)
+	result.Scan(&tableExist)
+	assert.Equal(t, true, tableExist)
+}
+
+func TestWithTxnRollback(t *testing.T) {
+	if _, err := client.Exec(dropTableQuery); err != nil {
+		log.Fatalf("Could not cleanup: %s", err)
+	}
+	query2 := "WRONG QUERY"
+
+	err := client.WithTxn(context.Background(), sql.TxOptions{}, func(tx *sqlx.Tx) error {
+		if _, err := tx.Exec(createTableQuery); err != nil {
+			return err
+		}
+		if _, err := tx.Exec(query2); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	// WithTx should return an error
+	assert.Error(t, err)
+
+	// Table should not be created
+	var tableExist bool
+	result := client.QueryRow(checkTableQuery)
+	result.Scan(&tableExist)
+	assert.Equal(t, false, tableExist)
 }
