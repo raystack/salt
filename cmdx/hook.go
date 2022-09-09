ff73eb2bf4ccff86d24a8a5e0cbd390cc8b682e0
Muhammad Abduh <mabdh@users.noreply.github.com>
2022-09-09 20:28:26 +0700

    fix(cmdx): recursion on hook

--- cmdx/hook.go
+++ cmdx/hook.go
@@ -5,10 +5,10 @@ import "github.com/spf13/cobra"
 // SetClientHook applies custom cobra config specific
 // for client or cmd that have annotation `client:true`
 func SetClientHook(rootCmd *cobra.Command, applyFunc func(cmd *cobra.Command)) {
-	for _, c := range rootCmd.Commands() {
-		if c.Annotations != nil && c.Annotations["client"] == "true" {
-			applyFunc(c)
+	for _, subCmd := range rootCmd.Commands() {
+		if subCmd.Annotations != nil && subCmd.Annotations["client"] == "true" {
+			applyFunc(subCmd)
 		}
-		SetClientHook(rootCmd, applyFunc)
+		SetClientHook(subCmd, applyFunc)
 	}
 }
