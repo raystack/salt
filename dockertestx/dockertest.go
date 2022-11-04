d87cefdf8799f5302a9335ea2f2af644efdba8ef
Muhammad Abduh <mabdh@users.noreply.github.com>
2022-11-04 14:55:57 +0700

    refactor: simplify dockertestx

--- dockertestx/dockertest.go
+++
@@ -1,9 +0,0 @@
-package dockertestx
-
-import "github.com/ory/dockertest/v3"
-
-// type and function aliasing to avoid conflicting dockertest name
-type Pool = dockertest.Pool
-type Resource = dockertest.Resource
-
-var NewPool = dockertest.NewPool
